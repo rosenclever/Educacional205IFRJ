7ebc2810de71f5961edb007a4402f308e178955e
Aula <Aula@CANP-LAB7-C01>
2025-11-07 10:49:27 -0300

    Adicionar arquivos de projeto.

---
+++ Academico/Data/AcademicoContext.cs
@@ -0,0 +1,14 @@
+﻿using Academico.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academico.Data
+{
+    public class AcademicoContext : DbContext
+    {
+        public AcademicoContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
